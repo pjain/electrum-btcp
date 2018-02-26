7f88dd60e132863ac1b2f13087513eb0caa0a614
Jon Layton <jon@seequ.com>
2018-02-26 11:50:12 -0600

    Build for Mac now makes .app named Electrum BTCP, clearer dmg/installer

--- create-dmg.sh
+++ create-dmg.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
+
 sudo sh ./clean.sh
+
 VERSION=$(cat lib/version.py \
   | grep ELECTRUM_VERSION \
   | sed "s/[',]//g" \
   | tr -d '[[:space:]]')
 VERSION=${VERSION//ELECTRUM_VERSION=/}
+
 echo "Creating package $VERSION"
 
 echo "brew install"
@@ -13,14 +16,14 @@ brew bundle
 echo "pip install"
 pip3 install -r requirements.txt
 
-echo "building icons"
+echo "Building icons"
 pyrcc5 icons.qrc -o gui/qt/icons_rc.py
 
-echo "Compile the protobuf description file"
+echo "Compiling the protobuf description file"
 protoc --proto_path=lib/ --python_out=lib/ lib/paymentrequest.proto
 
-echo "compiling translations"
-./config/make_locale
+echo "Compiling translations"
+./contrib/make_locale
 
 echo "Creating package $VERSION"
 sudo python3 setup.py sdist
@@ -28,7 +31,7 @@ sudo python3 setup.py sdist
 echo "Creating python app using py2app"
 sudo ARCHFLAGS="-arch i386 -arch x86_64" sudo python3 setup-release.py py2app --includes sip
 
-echo "Creating python Electrum.app and .dmg"
-sudo hdiutil create -fs HFS+ -volname "Electrum" -srcfolder dist/Electrum.app dist/electrum-$VERSION-macosx.dmg
+echo "Creating dist/Electrum.app and .dmg"
+sudo hdiutil create -fs HFS+ -volname "Electrum BTCP - Installer" -srcfolder "dist/Electrum BTCP.app" dist/electrum-$VERSION-macosx.dmg
 
 echo "Done!"
